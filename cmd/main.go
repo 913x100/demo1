351d5dbfecb8a374118ff96bc74859e2757cf1d6
Kittinut Iamsamarng <kittinut.l@ku.th>
2020-07-21 15:47:16 +0700

    add inner package

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/913x100/demo1/external/outer"
+	"github.com/913x100/demo1/internal/inner"
 )
 
 func main() {
 	o := outer.Outer{}
-
+	i := inner.Inner{}
 	o.Print()
+	i.Print()
 }
